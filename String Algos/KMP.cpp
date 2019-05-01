acd5906765f3480411568fdbad2d62005ae370fa
sidcoool <32618581+sidcoool@users.noreply.github.com>
2019-05-01 10:17:14 +0530

    Update KMP.cpp

--- String Algos/KMP.cpp
+++ String Algos/KMP.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
+
 void strNum(int arr[], string str, int l)
 {
     arr[0] = 0;
+
     int i = 0;
+
     for(int j=1; j<l; j++)
     {
         if(str[i] == str[j])
         {
+
             arr[j] = i + 1;
+
             i++;
+
         }
         else
         {
             while(i!=0 && str[i]!=str[j])
             {
                 i = arr[i-1];
+
                 if(str[i] == str[j])
+
                 {
+
                     arr[j] = i + 1;
+
                 }
             }
         }
@@ -26,44 +36,57 @@ void strNum(int arr[], string str, int l)
 }
 
 int main() {
-    string pattern = "abcdabca";
-    string text = "abcabcdabccaaa";
+    string pattern = "aabab";
+
+    string text = "aaababaabaababaab";
+
     int lp = pattern.length();
+
     int lt = text.length();
 
-    int arr[lp] = {0};
+    int arr[lp];
+    for(int i=0; i<lp; i++)
+    arr[i] = 0;
+
     strNum(arr,pattern,lp);
+    cout<<"strNum array is ";
+     for(int i=0; i<lp; i++)
+      cout<<arr[i]<<" ";
+      cout<<endl;
 
     int j = 0;
-    bool found = false;
+
 
     for(int i=0; i<lt; i++)
     {
-        if(found)
-        break;
-        
         if(text[i] == pattern[j])
         {
             j++;
             if(j == lp)
             {
-                cout<<"Pattern found !";
-                found = true;
-                break;
+
+                cout<<"Pattern found ! From index "<<i+1-lp<<" to "<<"index "<<i<<endl;
+                j = 0;
+
             }
+
         }
+
         else
         {
             while(j!=0)
             {
                 j = arr[j-1];
+
                 if(text[i] == pattern[j])
                 {
                     j++;
+
                     if(j == lp)
                     {
-                        cout<<"Pattern found !";
-                        found = true;
+
+                        cout<<"Pattern found ! From index "<<i+1-lp<<" to "<<"index "<<i<<endl;
+                        j = 0;
                     }
                     break;
                 }
@@ -71,10 +94,9 @@ int main() {
         }
     }
 
-    if(!found)
-    {
-        cout<<"Pattern Not Found !";
-    }
+
+
+
 
     return 0;
 }
